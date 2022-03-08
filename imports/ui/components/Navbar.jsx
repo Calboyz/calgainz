4de71b3339f6c773e7adb93311b285994ad9ad86
skyler8 <skyler8@hawaii.edu>
2022-03-08 05:55:53 -1000

    Show user that is logged in and is able to logout and added. Also added protected routes

--- imports/ui/components/Navbar.jsx
+++ imports/ui/components/Navbar.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import PropTypes from 'prop-types';
+import { Meteor } from 'meteor/meteor';
+import { withTracker } from 'meteor/react-meteor-data';
 import { Menu, Header, Dropdown } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
 
-export const Navbar = () => {
+const Navbar = ({ currentUser }) => {
     return (
         <Menu attached="top" borderless inverted>
             <Menu.Item as={NavLink} exact to="/">
@@ -10,13 +13,35 @@ export const Navbar = () => {
             </Menu.Item>
             <Menu.Item as={NavLink} to="/test">Test</Menu.Item>
             <Menu.Item position="right">
-                <Dropdown text="Login" pointing="top right" icon={'user'}>
-                    <Dropdown.Menu>
-                        <Dropdown.Item icon="user" text="Sign in" as={NavLink} exact to="/signin"/>
-                        <Dropdown.Item icon="add user" text="Register" as={NavLink} to="/register" />
-                    </Dropdown.Menu>
-                </Dropdown>
+                {currentUser === '' ? (
+                    <Dropdown text="Login" pointing="top right" icon={'user'}>
+                        <Dropdown.Menu>
+                            <Dropdown.Item icon="user" text="Sign in" as={NavLink} exact to="/signin"/>
+                            <Dropdown.Item icon="add user" text="Register" as={NavLink} to="/register" />
+                        </Dropdown.Menu>
+                    </Dropdown>
+                    ) : (
+                        <Dropdown text={currentUser} pointing="top right" icon='user'>
+                            <Dropdown.Menu>
+                                <Dropdown.Item icon='sign out' text='Sign Out' as={NavLink} exact to='/signout' />
+                            </Dropdown.Menu>
+                        </Dropdown>
+                )}
             </Menu.Item>
         </Menu>
     )
 }
+
+Navbar.propTypes =
+    {
+        currentUser: PropTypes.string,
+    };
+
+const NavbarContainer = withTracker(() => {
+    const currentUser = Meteor.user() ? Meteor.user().username : '';
+    return {
+        currentUser,
+    };
+})(Navbar);
+
+export default NavbarContainer;
--- imports/ui/layout/App.jsx
+++ imports/ui/layout/App.jsx
@@ -1,27 +1,42 @@
 import React from 'react';
+import { Meteor } from 'meteor/meteor';
 import {BrowserRouter as Router} from "react-router-dom";
-import {Routes, Route} from "react-router-dom";
+import {Routes, Route, Navigate, useLocation, Outlet} from "react-router-dom";
 import 'semantic-ui-css/semantic.css';
-import {Navbar} from "../components/Navbar";
-import {Landing} from "../pages/Landing";
-import {Test} from "../pages/Test";
-import {NotFound} from "../pages/404";
-import {Signin} from "../pages/signin";
-import {Register} from "../pages/register";
-import {ProfilePage} from "../pages/ProfilePage";
+import NavbarContainer from "../components/Navbar";
+import Landing from "../pages/Landing";
+import Test from "../pages/Test";
+import NotFound from "../pages/404";
+import Signin from "../pages/Signin";
+import Register from "../pages/Register";
+import ProfilePage from "../pages/ProfilePage";
+import Signout from "../pages/Signout";
 
 export const App = () => (
   <div>
     <Router>
-        <Navbar />
+        <NavbarContainer />
         <Routes>
             <Route path="/" element={<Landing />} />
-            <Route path="/profilepage" element={<ProfilePage/>}/>
             <Route path="/test" element={<Test />} />
             <Route path="/signin" element={<Signin />} />
             <Route path="/register" element={<Register />} />
             <Route path="*" element={<NotFound />} />
+            <Route path="/signout" element={<Signout />} />
+            <Route element={<ProtectedRoute />}>
+                <Route path="/profilepage" element={<ProfilePage />}/>
+            </Route>
         </Routes>
     </Router>
   </div>
 );
+
+const ProtectedRoute = () => {
+    const isLogged = Meteor.userId() !== null;
+    const location = useLocation();
+
+    if (!isLogged) {
+        return <Navigate to='/login' state={{ from: location }} />;
+    }
+    return <Outlet />
+}
